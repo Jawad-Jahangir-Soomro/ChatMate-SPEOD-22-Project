1529f8853bec5d309691d665c6b406ab95660647
Zohaib234 <21sw031@students.muet.edu.pk>
2022-08-27 10:02:47 +0500

    almost version ready

--- server/server.js
+++ server/server.js
@@ -3,17 +3,37 @@ const app= express();
 const server = require('http').createServer(app);
 const dotenv = require('dotenv').config();
 const cors = require('cors')
-const io = require('socket.io')(server);
+const {Server} = require('socket.io')
 const PORT = process.env.PORT || 3000;
-
-
 app.use(cors());
+
+const io = new Server (server,{
+    cors : {
+        origin:"http://localhost:3000",
+        methods : ["GET","POST"],
+    }
+})
+io.on('connection',(socket)=>{
+    console.log("user connected");
+    console.log(socket.id);
+     socket.on('join',data=>{
+        socket.join(data) 
+        console.log(`user with the id ${socket.id} just joind the room : ${data}`);
+     })
+
+     socket.on('send_message',data=>{
+        socket.to(data.room).emit('receive_message',data)
+     })
+    socket.on("disconnect",()=>{
+        console.log(` ${socket.id} leaved the chat `);
+    })
+})
 app.get('/',(req,res)=>{
     res.send('hello there ')
 })
-io.on('connection',socket=>{
-    console.log("socket io works");
-})
+
+
+
 server.listen(PORT,()=>{
     console.log(`server is responding on http://localhost:${process.env.PORT}`);
 })
